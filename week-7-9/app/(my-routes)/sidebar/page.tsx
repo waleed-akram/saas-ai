407fc83007a3160a124c2aac3073b0f43ee39c1a
waleed-akram <waleed.akram42069@gmail.com>
2025-05-12 20:15:52 +0500

    minor adjustments in the UI of sidebar

--- week-7-9/app/(my-routes)/sidebar/page.tsx
+++ week-7-9/app/(my-routes)/sidebar/page.tsx
@@ -1,13 +1,16 @@
 import Link from "next/link";
 import { RiRobot3Line } from "react-icons/ri";
+import { FaComments, FaMagic } from "react-icons/fa";
 
 export default function SideBar() {
-  
   return (
     <div className="grid col-span-2 row-span-4 w-full h-full breakpoints-sidebar pl-2 bg-gray-100">
       <div className="mt-10 text-4xl break-normal">
         <div className=" inline bg-white rounded-full mr-0.5">
-          <RiRobot3Line className="inline -mr-2 mb-1 bg-blue-500 text-white p-1.5 rounded-full" size={40} />{" "}
+          <RiRobot3Line
+            className="inline -mr-2 mb-1 bg-blue-500 text-white p-1.5 rounded-full"
+            size={40}
+          />{" "}
         </div>
         <Link href="/" className="text-gray-700 ">
           SaaS AI
@@ -16,9 +19,11 @@ export default function SideBar() {
       <div className="flex flex-col gap-1 px-1 -mt-50 mr-1.5">
         <span className="text-3xl font-bold p-1 ">~Tools~</span>
         <Link href="/response-section/firstTool" className="btn">
+          <FaComments className="text-xl inline mr-2 text-white" />
           Chat buddy
         </Link>
         <Link href="/response-section/secondTool" className="btn">
+          <FaMagic className="text-xl inline mr-2 text-white" />
           Imagine
         </Link>
       </div>
