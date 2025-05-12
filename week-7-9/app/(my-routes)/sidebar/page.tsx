95a702906b70161c9e159da11ce9352552525cd3
waleed-akram <waleed.akram42069@gmail.com>
2025-05-12 20:25:34 +0500

    minor adjustments in UI of side bar

--- week-7-9/app/(my-routes)/sidebar/page.tsx
+++ week-7-9/app/(my-routes)/sidebar/page.tsx
@@ -4,11 +4,11 @@ import { FaComments, FaMagic } from "react-icons/fa";
 
 export default function SideBar() {
   return (
-    <div className="grid col-span-2 row-span-4 w-full h-full breakpoints-sidebar pl-2 bg-gray-100">
+    <div className="min-h-screen grid col-span-2 row-span-4 w-full breakpoints-sidebar pl-2 bg-gray-100">
       <div className="mt-10 text-4xl break-normal">
         <div className=" inline bg-white rounded-full mr-0.5">
           <RiRobot3Line
-            className="inline -mr-2 mb-1 bg-blue-500 text-white p-1.5 rounded-full"
+            className="inline mb-1 bg-blue-500 text-white p-1.5 rounded-full"
             size={40}
           />
         </div>
